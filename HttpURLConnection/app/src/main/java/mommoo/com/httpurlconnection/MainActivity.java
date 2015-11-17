1c2622213e539f1b4f9ccca8940ed15fd072cb80
Mommoo <nova112211@gmail.com>
2015-11-17 12:29:46 +0900

    urlconnection 완료

--- HttpURLConnection/app/src/main/java/mommoo/com/httpurlconnection/MainActivity.java
+++ HttpURLConnection/app/src/main/java/mommoo/com/httpurlconnection/MainActivity.java
@@ -1,5 +1,6 @@
 package mommoo.com.httpurlconnection;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,7 +10,21 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
 public class MainActivity extends AppCompatActivity {
+
+    private URL Url;
+    private String strUrl,strCookie,result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,35 +33,50 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        new AsyncTask<Void,Void,Void>(){
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            protected void onPreExecute() {
+                super.onPreExecute();
+                strUrl = "http://www.naver.com"; //탐색하고 싶은 URL이다.
             }
-        });
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+            @Override
+            protected Void doInBackground(Void... voids) {
+                try{
+                    Url = new URL(strUrl);  // URL화 한다.
+                    HttpURLConnection conn = (HttpURLConnection) Url.openConnection(); // URL을 연결한 객체 생성.
+                    conn.setRequestMethod("GET"); // get방식 통신
+                    conn.setDoOutput(true);       // 쓰기모드 지정
+                    conn.setDoInput(true);        // 읽기모드 지정
+                    conn.setUseCaches(false);     // 캐싱데이터를 받을지 안받을지
+                    conn.setDefaultUseCaches(false); // 캐싱데이터 디폴트 값 설정
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+                    strCookie = conn.getHeaderField("Set-Cookie"); //쿠키데이터 보관
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+                    InputStream is = conn.getInputStream();        //input스트림 개방
 
-        return super.onOptionsItemSelected(item);
+                    StringBuilder builder = new StringBuilder();   //문자열을 담기 위한 객체
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(is,"UTF-8"));  //문자열 셋 세팅
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        builder.append(line+ "\n");
+                    }
+
+                    result = builder.toString();
+                }catch(MalformedURLException | ProtocolException  exception) {
+                    exception.printStackTrace();
+                }catch(IOException io){
+                    io.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                System.out.println(result);
+            }
+        }.execute();
     }
 }
